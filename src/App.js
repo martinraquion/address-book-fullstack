8c1e8da07398b52380e884757295af2b7428c6b6
Martin Raquion <raquionearl@gmail.com>
2019-07-29 11:45:56 +0800

    Initialize Sign In and Sign Up page with Material UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SignIn from './components/SignIn'
+import Register from './components/Register'
+function App() {
+  return (
+    <React.Fragment>
+        <SignIn />
+        {/* <Register /> */}
+    </React.Fragment>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/components/Register.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+// import Paper from '@material-ui/core/Paper'
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Register() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+     
+        <Typography component="h1" variant="h5">
+            Sign Up
+        </Typography>
+        <form className={classes.form} validate>
+        <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            name="firstName"
+            autoFocus
+          />
+          
+        <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+          />
+            <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+          />
+        
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+   
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+          >
+            Register
+          </Button>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+            }}>
+                <Link href="#">
+                {"Already have an Account? Sign In"}
+              </Link>
+            </div>
+        </form>
+      </div>
+   
+    </Container>
+  );
+}
